d1cdab749cd8281ae9abd71ad013e4ef343ae201
Alexandre Angrignon <alex040892@gmail.com>
2025-08-20 14:53:35 +1200

    importing and displaying data

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import exercises from './assets/data/exercises.json';
 export default function App() {
+  const exercise = exercises[2];
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      {/* <FlatList
+      data={exercises}
+      /> */}
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{exercise.name} </Text>
+        <Text style={styles.exerciseSubtitle}>
+          Muscle: {exercise.muscle.toUpperCase()} | Equipment:{' '}
+          {exercise.equipment.toUpperCase()}
+        </Text>
+      </View>
       <StatusBar style='auto' />
     </View>
   );
@@ -13,8 +23,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
+    padding: '10',
+  },
+  exerciseName: {
+    fontSize: 24,
+    fontWeight: '500',
+  },
+  exerciseSubtitle: {
+    color: 'dimgray',
+  },
+  exerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    gap: 5,
   },
 });
